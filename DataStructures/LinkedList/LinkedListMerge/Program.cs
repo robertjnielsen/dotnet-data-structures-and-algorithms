9c1bd489c5315e2ca5038b677493f4be84954585
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-26 11:26:09 -0700

    LinkedListMerge project created.

---
+++ DataStructures/LinkedList/LinkedListMerge/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LinkedList.Classes;
+
+namespace LinkedListMerge
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static LinkList MergeLists(LinkList list1, LinkList list2)
+        {
+            return list1;
+        }
+    }
+}
