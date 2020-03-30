3e4c1c0e113d0fdd2d21fd734340bef38ba5b3db
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-30 11:34:27 -0700

    Test CanSuccessfullyDequeueFromAQueue passed.

--- DataStructures/StacksAndQueues/QueueTests/QueueTest.cs
+++ DataStructures/StacksAndQueues/QueueTests/QueueTest.cs
@@ -56,5 +56,18 @@ namespace QueueTests
 
             Assert.Equal(expected, testQueue.Front.Value);
         }
+
+        [Fact]
+        public void CanSuccessfullyDequeueFromAQueue()
+        {
+            Queue testQueue = new Queue();
+            testQueue.Enqueue(3);
+            testQueue.Enqueue(9);
+            testQueue.Enqueue(19);
+
+            int expected = 3;
+
+            Assert.Equal(expected, testQueue.Dequeue());
+        }
     }
 }
