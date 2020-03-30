1a3a1e56fccf8f1a5e00ffaae866e93201c4bea1
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-30 10:58:04 -0700

    Queue.Enqueue method complete.

--- DataStructures/StacksAndQueues/StacksAndQueues/Classes/Queue.cs
+++ DataStructures/StacksAndQueues/StacksAndQueues/Classes/Queue.cs
@@ -14,7 +14,23 @@ namespace StacksAndQueues.Classes
         /// <summary>
         /// Determines the Rear Node in the Queue.
         /// </summary>
-        private Node Rear { get; set; }
+        private Node Rear { get; set; } = null;
+
+        /// <summary>
+        /// Adds a new Node to the Rear of the Queue.
+        /// </summary>
+        /// <param name="value">The value passed to the new Node object.</param>
+        public void Enqueue(int value)
+        {
+            // Instantiate a new Node object with the given value.
+            Node newNode = new Node(value);
+
+            // Tell the current Rear Node to reference the new Node.
+            Rear.Next = newNode;
+
+            // Tell the Queue that the new Node is now the Rear.
+            Rear = newNode;
+        }
 
         /// <summary>
         /// Determines if the Queue is empty or not.
