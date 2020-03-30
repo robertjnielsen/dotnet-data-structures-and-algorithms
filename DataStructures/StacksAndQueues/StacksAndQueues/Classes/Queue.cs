268ce81acd5f3fe705536eba9e1a221ab76722b2
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-30 10:48:06 -0700

    Queue class created. Queue.IsEmpty method created.

---
+++ DataStructures/StacksAndQueues/StacksAndQueues/Classes/Queue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues.Classes
+{
+    public class Queue
+    {
+        /// <summary>
+        /// Determines the Front Node in the Queue.
+        /// </summary>
+        public Node Front { get; set; } = null;
+
+        /// <summary>
+        /// Determines the Rear Node in the Queue.
+        /// </summary>
+        private Node Rear { get; set; }
+
+        /// <summary>
+        /// Determines if the Queue is empty or not.
+        /// </summary>
+        /// <returns>Returns a boolean value determined by whether or not the Queue is empty.</returns>
+        public bool IsEmpty()
+        {
+            return Front == null;
+        }
+    }
+}
