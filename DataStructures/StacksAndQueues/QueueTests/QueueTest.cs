8061949d73b8e20b593db9f73e2b23ab7052fc77
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-30 11:09:40 -0700

    QueueTests project created.

---
+++ DataStructures/StacksAndQueues/QueueTests/QueueTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using StacksAndQueues.Classes;
+
+namespace QueueTests
+{
+    public class QueueTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
