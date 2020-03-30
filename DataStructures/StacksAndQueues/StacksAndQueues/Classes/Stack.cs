922ce9b949976c34b3baf32013ebf7c9dbeab53d
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-30 09:58:37 -0700

    StacksAndQueues project created. Node class created.

---
+++ DataStructures/StacksAndQueues/StacksAndQueues/Classes/Stack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues.Classes
+{
+    public class Stack
+    {
+        public Node Top { get; set; } = null;
+
+        public void Push(int value)
+        {
+            Node newNode = new Node(value);
+
+
+        }
+    }
+}
