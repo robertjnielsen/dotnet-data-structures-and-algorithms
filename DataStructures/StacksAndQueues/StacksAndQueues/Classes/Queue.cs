cd32e06e30e9dba56c4b877759a0a904efac263e
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-30 11:02:54 -0700

    Queue.Dequque method complete.

--- DataStructures/StacksAndQueues/StacksAndQueues/Classes/Queue.cs
+++ DataStructures/StacksAndQueues/StacksAndQueues/Classes/Queue.cs
@@ -33,6 +33,30 @@ namespace StacksAndQueues.Classes
         }
 
         /// <summary>
+        /// Removes the Front Node from the Queue.
+        /// </summary>
+        /// <returns>Returns the Value of the current Front Node.</returns>
+        public int Dequeue()
+        {
+            // Throw an Exception if the Queue is empty.
+            if (IsEmpty())
+            {
+                throw new Exception("Cannot Dequeue! The Queue is empty.");
+            }
+
+            // Create a temporary reference to the Front Node.
+            Node temp = Front;
+
+            // Tell the current Front Node to reference the Next Node in the Queue.
+            Front = Front.Next;
+
+            // Remove any Next reference from the temporary Node.
+            temp.Next = null;
+
+            return temp.Value;
+        }
+
+        /// <summary>
         /// Determines if the Queue is empty or not.
         /// </summary>
         /// <returns>Returns a boolean value determined by whether or not the Queue is empty.</returns>
