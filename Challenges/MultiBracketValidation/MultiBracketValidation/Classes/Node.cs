5ac50841025b43aa3ff37b980f8cea67f113f98d
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-04-02 10:11:08 -0700

    Node and Stack classes created.

---
+++ Challenges/MultiBracketValidation/MultiBracketValidation/Classes/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiBracketValidation.Classes
+{
+    public class Node
+    {
+        public char Value { get; set; }
+
+        public Node Next { get; set; }
+
+        public Node(char value)
+        {
+            this.Value = value;
+        }
+    }
+}
