d063dff71d883439550cc3433dd910c19e5eaaef
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-04-02 10:50:48 -0700

    Unit tests project created.

---
+++ Challenges/MultiBracketValidation/BracketValidationTests/BracketTest.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using MultiBracketValidation;
+using MultiBracketValidation.Classes;
+
+namespace BracketValidationTests
+{
+    public class BracketTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
