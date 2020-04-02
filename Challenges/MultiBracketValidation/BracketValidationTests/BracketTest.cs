c2cbabc4cb9b772c9b0e9441151e10aa368884ae
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-04-02 10:56:53 -0700

    Test HelperMethodCanSuccessfullyValidateBrackets passed.

--- Challenges/MultiBracketValidation/BracketValidationTests/BracketTest.cs
+++ Challenges/MultiBracketValidation/BracketValidationTests/BracketTest.cs
@@ -8,9 +8,14 @@ namespace BracketValidationTests
     public class BracketTest
     {
         [Fact]
-        public void Test1()
+        public void HelperMethodCanSuccessfullyValidateBrackets()
         {
+            char open = '{';
+            char close = '}';
 
+            bool validated = Program.ValidateOnPop(open, close);
+
+            Assert.True(validated);
         }
     }
 }
