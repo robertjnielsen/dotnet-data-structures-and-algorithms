29b5edabee5622c649309c9b6b6df87d12e2303f
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-04-06 09:46:18 -0700

    Node class created.

---
+++ DataStructures/Trees/Trees/Classes/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees.Classes
+{
+    public class Node
+    {
+        public int Value { get; set; }
+
+        public Node LeftChild { get; set; }
+
+        public Node RightChild { get; set; }
+
+    }
+}
