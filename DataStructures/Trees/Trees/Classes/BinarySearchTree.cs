530c6dbd281d4633d2720d402c372703823d7fe5
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-04-06 10:40:35 -0700

    Add method complete.

--- DataStructures/Trees/Trees/Classes/BinarySearchTree.cs
+++ DataStructures/Trees/Trees/Classes/BinarySearchTree.cs
@@ -6,14 +6,45 @@ namespace Trees.Classes
 {
     public class BinarySearchTree : BinaryTree
     {
+        /// <summary>
+        /// Constructor to create an empty instance.
+        /// </summary>
         public BinarySearchTree()
         {
             Root = null;
         }
 
+        /// <summary>
+        /// Constructor to create an instance with a new Root Node.
+        /// </summary>
+        /// <param name="value">The value to pass into the new Root Node.</param>
         public BinarySearchTree(int value)
         {
             Root = new Node(value);
+        }
+
+        /// <summary>
+        /// Adds a new Node to the Binary Search Tree in the correct location.
+        /// </summary>
+        /// <param name="root">The Node used to locate the correct position to add a new Node.</param>
+        /// <param name="value">The value to pass to the new Node being added.</param>
+        /// <returns>Returns the Node being passed for recursion.</returns>
+        public Node Add(Node root, int value)
+        {
+            if (root == null)
+            {
+                root = new Node(value);
+            }
+            else if (value < root.Value)
+            {
+                root.LeftChild = Add(root.LeftChild, value);
+            }
+            else
+            {
+                root.RightChild = Add(root.RightChild, value);
+            }
+
+            return root;
         }
     }
 }
