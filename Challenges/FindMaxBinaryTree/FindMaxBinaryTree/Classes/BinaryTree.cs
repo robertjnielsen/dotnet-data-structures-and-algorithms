9bcc069d1b0e2def0f0b10771a144ebcd92032e5
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-04-09 09:40:09 -0700

    Added BinaryTree class.

---
+++ Challenges/FindMaxBinaryTree/FindMaxBinaryTree/Classes/BinaryTree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaxBinaryTree.Classes
+{
+    public class BinaryTree
+    {
+        /// <summary>
+        /// The Root Node of the Tree.
+        /// </summary>
+        public Node Root { get; set; }
+
+        /// <summary>
+        ///  Instantiates an empty Tree.
+        /// </summary>
+        public BinaryTree()
+        {
+
+        }
+
+        /// <summary>
+        /// Instantiates a new Tree with a Root Node.
+        /// </summary>
+        /// <param name="root"></param>
+        public BinaryTree(Node root)
+        {
+            Root = root;
+        }
+    }
+}
