2f606f4a824547fd5b6e655087745b764682b800
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-04-09 10:55:59 -0700

    Final test not passed.

--- Challenges/FindMaxBinaryTree/FindMaxBinaryTreeTests/UnitTest1.cs
+++ Challenges/FindMaxBinaryTree/FindMaxBinaryTreeTests/UnitTest1.cs
@@ -38,5 +38,23 @@ namespace FindMaxBinaryTreeTests
 
             Assert.Equal(expected, maxValue);
         }
+
+        [Fact]
+        public void CanReturnMaxValueOfTreeWithMultipleNodes()
+        {
+            Node node = new Node(0);
+            BinaryTree tree = new BinaryTree(node);
+            tree.Add(tree.Root, 3);
+            tree.Add(tree.Root, 9);
+            tree.Add(tree.Root, 3);
+            tree.Add(tree.Root, 19);
+            tree.Add(tree.Root, 3);
+            tree.Add(tree.Root, 27);
+
+            int expected = 27;
+            int maxValue = Program.FindMaxBinaryTree(tree);
+
+            Assert.Equal(expected, maxValue);
+        }
     }
 }
