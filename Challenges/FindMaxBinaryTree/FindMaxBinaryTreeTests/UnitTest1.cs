3b10a2e7cf4c0b0cedf23073b0523b7526a88868
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-04-09 09:43:54 -0700

    Test CanInstantiateAnEmptyTree passed.

--- Challenges/FindMaxBinaryTree/FindMaxBinaryTreeTests/UnitTest1.cs
+++ Challenges/FindMaxBinaryTree/FindMaxBinaryTreeTests/UnitTest1.cs
@@ -1,14 +1,17 @@
 using System;
 using Xunit;
+using FindMaxBinaryTree.Classes;
 
 namespace FindMaxBinaryTreeTests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CanInstantiateAnEmptyTree()
         {
+            BinaryTree tree = new BinaryTree();
 
+            Assert.Null(tree.Root);
         }
     }
 }
