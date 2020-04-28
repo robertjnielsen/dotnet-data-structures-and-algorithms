82101cf0aa304b300a894a6b2988d491c0755a91
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-04-27 23:21:12 -0700

    Test CanSuccessfullyHashAKeyToAnInRangeValue passed.

--- DataStructures/HashTable/HashTableTests/UnitTest1.cs
+++ DataStructures/HashTable/HashTableTests/UnitTest1.cs
@@ -1,14 +1,20 @@
 using System;
 using Xunit;
+using HashTables;
+using HashTables.Classes;
 
 namespace HashTableTests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CanSuccessfullyHashAKeyToAnInRangeValue()
         {
+            HashTable table = new HashTable(1024);
 
+            int value = table.Hash("test");
+
+            Assert.True(value < table.Size);
         }
     }
 }
