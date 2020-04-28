9c3929c07b36bf6d3856305e974397a9a0d2299c
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-04-27 23:37:02 -0700

    Test CanRetrieveAValueFromABucketWithCollisions passed.

--- DataStructures/HashTable/HashTableTests/UnitTest1.cs
+++ DataStructures/HashTable/HashTableTests/UnitTest1.cs
@@ -30,5 +30,23 @@ namespace HashTableTests
 
             Assert.Equal(value1, value2);
         }
+
+        [Fact]
+        public void CanRetrieveAValueFromABucketWithCollisions()
+        {
+            HashTable table = new HashTable(1024);
+
+            table.Add("abc", "Some text.");
+            table.Add("cba", "Some other text.");
+
+            string result1 = table.Get("abc");
+            string result2 = table.Get("cba");
+
+            string expected1 = "Some text.";
+            string expected2 = "Some other text.";
+
+            Assert.Equal(expected1, result1);
+            Assert.Equal(expected2, result2);
+        }
     }
 }
